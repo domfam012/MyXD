bcc740709995cb121fb26e4b7e566307b92f7cff
pyungwook-domfam <60639032+domfam012@users.noreply.github.com>
2020-02-06 09:48:44 +0900

    -admin/list tempalte

--- pages/admin/p/list.js
+++ pages/admin/p/list.js
@@ -10,9 +10,28 @@ const List = props => {
 
             <h1>Admin List Page</h1>
 
+            <div className={"content"}>
+                <div className={"row box"}>
+                    <div className={"col col-sm-12"}>
+
+                        <h1 className={"header"}>XD 올리기</h1>
+
+                    </div>
+                </div>
+            </div>
+
             <style jsx>{`
-                h1 {
-                    font-color: #333;
+                .box {
+                    margin: 70px auto;
+                    border: 0 solid transparent;
+                    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+                }
+                .header {
+                    margin: 32px 35px 0;
+                    padding: 0 0 29px;
+                    font-weight: bold;
+                    line-height: 1;
+                    border-bottom: 3px solid #96959A;
                 }
             `}</style>
 
