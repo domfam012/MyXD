b009cc58f95435d297b5139ea599c3e05e995765
pyungwook-domfam <60639032+domfam012@users.noreply.github.com>
2020-02-06 11:06:49 +0900

    -bootstrap js, jquery js load

--- include/Layout.js
+++ include/Layout.js
@@ -16,12 +16,14 @@ const Layout = props => {
                 <title>MyXD</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport"/>
                 <meta name="Referrer" content="origin"/>
-                <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+                <meta httpEquiv="X-UA-Compatible" content="IE=Edge"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
                 <meta name="apple-mobile-web-app-title" content="MyXD"/>
                 <meta name="robots" content="index,nofollow"/>
                 <meta name="description" content="MyXD"/>
                 <meta name="keywords" content="MyXD"/>
+                <script src="/js/jquery.js"/>
+                <script src="/js/bootstrap.min.js"/>
             </Head>
             <Header/>
             <div className={isAdmin ? "container-xl" : "container"}>
