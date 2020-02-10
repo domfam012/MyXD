543774887b68c3cf0ccc680e27500ba13dae85a7
DCK-domfam <60648246+DCK-domfam@users.noreply.github.com>
2020-02-10 15:10:18 +0900

    commit

--- pages/api/board/create.js
+++ pages/api/board/create.js
@@ -1,4 +1,6 @@
 import { loadDB } from './../../../lib/js/db';
+import {now} from "moment";
+import index from "next/dist/export";
 
 export default async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -7,19 +9,26 @@ export default async (req, res) => {
     res.setHeader('Content-Type', 'application/json');
 
     console.log(`req.method : ${req.method}`);
+    console.log(req.body);
     if (req.method === 'POST') {
         const db = await loadDB();
         // const ref = await db.collection('Posts').post();
 
-        const category = '';
-        const content = 'sdfjkhasdlkfh';
-        // const created = 'sdfasdkjlf'; // -> 현재시각
-        const imgOriginName = 'sadfadsf';
-        const imgPath = 'asdfasdf';
-        const imgSaveName = 'asdfasdfg434';
-        const link = 0;
-        const title='aaaaaa';
-        const viewCount = 0;
+        const category = req.body.category;
+        const content = req.body.content;
+        const imgOriginName = req.body.imgOriginName;
+        const imgPath = 'c://///';
+        const imgSaveName = 'myXD-'+ imgOriginName;
+        const link = req.body.link;
+        const title= req.body.title;
+
+        const index = await db.collection('Posts').length+1;
+
+
+        console.log(category);
+
+        // 받아온 값 타입 && null 체크
+        //
 
         const data = {
             category: category,
@@ -28,18 +37,20 @@ export default async (req, res) => {
             imgPath: imgPath,
             imgSaveName: imgSaveName,
             link: link,
-            index: 0,
-            title: title
+            index: index,
+            title: title,
+            viewCount: 0,
+            created: ''
         };
 
-        console.log(data);
+        // console.log(data);
 
         const collection = db.collection('Posts');
         await collection.add(data);
         // return collection.add(data);
 
 
-        console.log(data);
+
 
         res.status(200).json({ status: 200, data: data });
 
