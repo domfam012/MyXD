f0ae2c06051e7cbc65d69dd23fb3bf95e18a3fb6
DCK-domfam <60648246+DCK-domfam@users.noreply.github.com>
2020-02-14 11:39:35 +0900

    localhost 테스트중 주연님건 pull

--- pages/p/[pid].js
+++ pages/p/[pid].js
@@ -235,10 +235,10 @@ Detail.getInitialProps = async ctx => {
 
     console.log("API연결 pid" + pid);
 
-    const res = await fetch(`http://13.209.55.219/api/board/post/${pid}`);
+    const res = await fetch(`http://localhost:3000/api/board/post/${pid}`);
     const result = await res.json();
 
-    const asideRes = await fetch('http://13.209.55.219/api/board/interest');
+    const asideRes = await fetch('http://localhost:3000/api/board/interest');
     const asideResult = await asideRes.json();
 
     console.log('Aside!!!!'+asideRes);
