8c7d3b151964a373b37a419092c194b6a8225e88
domfam <janielee0919@gmail.com>
2020-02-14 11:00:03 +0900

    - line-height

--- pages/admin/p/[pid]/update.js
+++ pages/admin/p/[pid]/update.js
@@ -124,6 +124,31 @@ const Update = props => {
                                             />
                                         </div>
                                     </div>
+
+                                    <div className={"form-group"}>
+                                        <div className={"label-area"}>
+                                            <label className="col-form-label">카테고리</label>
+                                        </div>
+                                        <div className={"input-area"}>
+                                            <label className="checkbox checkbox_single">
+                                                <input type="checkbox" className="filled-in"/>
+                                                <span>UI KITS</span>
+                                            </label>
+                                            <label className="checkbox checkbox_single">
+                                                <input type="checkbox" className="filled-in"/>
+                                                <span>Website</span>
+                                            </label>
+                                            <label className="checkbox checkbox_single">
+                                                <input type="checkbox" className="filled-in"/>
+                                                <span>Mobile</span>
+                                            </label>
+                                            <label className="checkbox checkbox_single">
+                                                <input type="checkbox" className="filled-in"/>
+                                                <span>Plug-in</span>
+                                            </label>
+                                        </div>
+                                    </div>
+
                                     <div className={"form-group"}>
                                         <div className={"label-area"}>
                                             <label className="col-form-label" style={{"lineHeight":"20.4"}}>내용</label>
