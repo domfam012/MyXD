baee8ed50cd12bda6f54e78c200437a14e1e3f11
domfam <janielee0919@gmail.com>
2020-02-21 14:13:30 +0900

    - 메인 페이지 SASS 적용

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,13 @@ import React from "react";
 import Layout from '../include/Layout';
 import Aside from '../include/Aside';
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import {useRouter} from "next/router";
 import Pagination from "react-js-pagination";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDoubleRight } from '@fortawesome/pro-light-svg-icons';
-import { faChevronDoubleLeft } from '@fortawesome/pro-light-svg-icons';
-import { faChevronLeft } from '@fortawesome/pro-light-svg-icons';
-import { faChevronRight } from '@fortawesome/pro-light-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faChevronDoubleRight} from '@fortawesome/pro-light-svg-icons';
+import {faChevronDoubleLeft} from '@fortawesome/pro-light-svg-icons';
+import {faChevronLeft} from '@fortawesome/pro-light-svg-icons';
+import {faChevronRight} from '@fortawesome/pro-light-svg-icons';
 import fetch from "isomorphic-unfetch";
 
 // card 컴포넌트
@@ -42,92 +41,89 @@ const Card = props => {
                     background-color: #ffffff;
                     margin-bottom: 50px;
                     cursor: pointer;
-                }
-                .main_card:last-child {
-                  margin-bottom: 0;
-                }
-                .main_card .img {
-                    width: 100%;
-                    height: 506px;
-                    background: #d3d3d3;
-                    text-align: center;
-                }
-                .main_card .img img {
-                    width: auto;
-                    max-width: 100%;
-                    height: 100%;
-                }
-                .box_text {
-                    padding: 33px 30px 30px 30px;
-                }
-                .box_text .title{
-                    font-size: 30px;
-                    line-height: 40px;
-                    letter-spacing: -1.5px;
-                    overflow: hidden;
-                    white-space: nowrap;
-                    text-overflow: ellipsis;
-                    color: #333333;
-                }
-                .box_text .text{
-                    font-size: 16px;
-                    line-height: 25px;
-                    color: #666666;
-                    margin: 23px 0 40px;
-                    display: -webkit-box;
-                    white-space: normal;
-                    height: 73px;
-                    word-wrap: break-word;
-                    -webkit-line-clamp: 3;
-                    -webkit-box-orient: vertical;
-                    text-overflow: ellipsis;
-                    overflow: hidden;
+                    :last-child {
+                        margin-bottom: 0;
                     }
-                .btn {
-                  color: #ffffff;
-                  line-height: 33px;
+                    .img {
+                        width: 100%;
+                        height: 506px;
+                        background: #d3d3d3;
+                        text-align: center;
+                        img {
+                            width: auto;
+                            max-width: 100%;
+                            height: 100%;
+                        }
+                    }
+                    .box_text {
+                            padding: 33px 30px 30px 30px;
+                        .title  {
+                            font-size: 30px;
+                            line-height: 40px;
+                            letter-spacing: -1.5px;
+                            overflow: hidden;
+                            white-space: nowrap;
+                            text-overflow: ellipsis;
+                            color: #333333;
+                        }
+                        .text{
+                            font-size: 16px;
+                            line-height: 25px;
+                            color: #666666;
+                            margin: 23px 0 40px;
+                            display: -webkit-box;
+                            white-space: normal;
+                            height: 73px;
+                            word-wrap: break-word;
+                            -webkit-line-clamp: 3;
+                            -webkit-box-orient: vertical;
+                            text-overflow: ellipsis;
+                            overflow: hidden;
+                        }
+                        .btn {
+                          color: #ffffff;
+                          line-height: 33px;
+                            :hover,:active {
+                              line-height: 33px;
+                            }
+                        }
+                    }
                 }
-                .btn:hover {
-                  line-height: 33px;
+                @media (max-width: 1200px) {
+                    .main_card {
+                        width: 100%;
+                        margin-right: 0;
+                            .img {
+                            width: 100%;
+                            height: 100%;
+                               img {
+                                width: 100%;
+                                height: 100%;
+                            }
+                        }
+                        .text {
+                        height: 70px;
+                      }
+                    }
                 }
-                .btn:active{
-                  line-height: 33px;
+                @media (max-width: 760px) {
+                    .box_text {
+                        padding: 30px 40px;
+                        .title{
+                            line-height: 48px;
+                            letter-spacing: -0.5px;
+                        }
+                        .text{
+                            font-size: 16px;
+                            line-height: 25px;
+                            letter-spacing: -0.5px;
+                            margin: 10px 0 40px;
+                        }
+                    }
+                    .btn {
+                      width: 100%;
+                    }
                 }
-            @media (max-width: 1200px) {
-                .main_card {
-                    width: 100%;
-                    margin-right: 0;
-                }
-                .main_card .img {
-                    width: 100%;
-                    height: 100%
-                }
-                .main_card .img img {
-                    width: 100%;
-                    height: 100%
-                }
-                .main_card .text {
-                    height: 70px;
-                }
-            }
-            @media (max-width: 760px) {
-                .box_text {
-                    padding: 30px 40px;
-                }
-                .box_text .title{
-                    line-height: 48px;
-                    letter-spacing: -0.5px;
-                }
-                .box_text .text{
-                    font-size: 16px;
-                    line-height: 25px;
-                    letter-spacing: -0.5px;
-                    margin: 10px 0 40px;
-                }
-                .btn {
-                  width: 100%;
-                }
-            }
             `}</style>
             </div>
         </Link>
@@ -155,7 +151,8 @@ const Index = props => {
                     {
                         props.data.map(item => (
                             //Card 컴포넌트
-                            <Card key={item.pid} title={item.title} content={item.content} imgPath={item.imgPath} pid={item.pid} link={item.link}/>
+                            <Card key={item.pid} title={item.title} content={item.content} imgPath={item.imgPath}
+                                  pid={item.pid} link={item.link}/>
                         ))
                     }
                     <div className={"nav"}>
@@ -174,15 +171,15 @@ const Index = props => {
                             linkClassNext="next"
                             linkClassPrev="prev"
                             linkClassFirst="first"
-                            firstPageText={<FontAwesomeIcon icon={faChevronDoubleLeft} />}
-                            prevPageText={<FontAwesomeIcon icon={faChevronLeft} />}
-                            nextPageText={<FontAwesomeIcon icon={faChevronRight} />}
-                            lastPageText={<FontAwesomeIcon icon={faChevronDoubleRight} />}
+                            firstPageText={<FontAwesomeIcon icon={faChevronDoubleLeft}/>}
+                            prevPageText={<FontAwesomeIcon icon={faChevronLeft}/>}
+                            nextPageText={<FontAwesomeIcon icon={faChevronRight}/>}
+                            lastPageText={<FontAwesomeIcon icon={faChevronDoubleRight}/>}
                         />
                     </div>
                 </div>
                 {/*aside 컴포넌트*/}
-                <Aside asideData={ props.asideData }/>
+                <Aside asideData={props.asideData}/>
             </div>
             <style jsx>{`
                 .nav {
@@ -211,7 +208,7 @@ Index.getInitialProps = async function (ctx) {
 
     return {
         data: result.data,
-        activePage : Number(page),
+        activePage: Number(page),
         asideData: asideResult.data,
         total: result.total
     }
