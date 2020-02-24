e5593f99a0511702c9ddfc7580d2c67ad3e6b8d7
pyungwook-domfam <pyungwook.hur@domfam.com>
2020-02-24 20:03:04 +0900

    -

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 const withCSS = require('@zeit/next-css');
 const { option } = require('./config.js');
 
-// module.exports = withCSS({});
-
 const assetPrefix = process.env.NODE_ENV === `production` ? option.assetPrefix.aws : option.assetPrefix.dev;
 module.exports = withCSS({
     distDir: '_next',
