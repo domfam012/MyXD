8d334330eecd775a37f467886e6cfab6b853b68b
pyungwook-domfam <pyungwook.hur@domfam.com>
2020-02-25 13:36:15 +0900

    - Update config.js

--- config.js
+++ config.js
@@ -1,27 +1,47 @@
+// live: {
+//     apiKey: "AIzaSyBDq0nTd2Czqy9XtOOOdjaWEcWwdmPV_Fw",
+//     authDomain: "myxd-live.firebaseapp.com",
+//     databaseURL: "https://myxd-live.firebaseio.com",
+//     projectId: "myxd-live",
+//     storageBucket: "myxd-live.appspot.com",
+//     messagingSenderId: "967461744534",
+//     appId: "1:967461744534:web:89ae4ed5d1d505a9dab7b7"
+// }
+
+// stage: {
+//     apiKey: "AIzaSyC_5Ia408Xj6EJ6aKbLzlOS_RHssy3R9uY",
+//     authDomain: "myxd-stage.firebaseapp.com",
+//     databaseURL: "https://myxd-stage.firebaseio.com",
+//     projectId: "myxd-stage",
+//     storageBucket: "myxd-stage.appspot.com",
+//     messagingSenderId: "864151577963",
+//     appId: "1:864151577963:web:185d4aaa421f8cb917982e"
+// }
+
 const option = {
     firestore: {
-        apiKey: "Your-Firebase-apiKey",
-        authDomain: "Your-Firebase-authDomain",
-        databaseURL: "Your-Firebase-databaseURL",
-        projectId: "Your-Firebase-projectId",
-        storageBucket: "Your-Firebase-storageBucket",
-        messagingSenderId: "Your-Firebase-messagingSenderId",
-        appId: "Your-Firebase-appId"
+        apiKey: "AIzaSyDyYie0HBPkZslWwib3DNBCIXM9VBMtL3s",
+        authDomain: "myxd-247f4.firebaseapp.com",
+        databaseURL: "https://myxd-247f4.firebaseio.com",
+        projectId: "myxd-247f4",
+        storageBucket: "myxd-247f4.appspot.com",
+        messagingSenderId: "575641093519",
+        appId: "1:575641093519:web:56035054fbaa244dfe37fa"
     },
     assetPrefix: {
         dev: 'http://localhost:3000',
-        aws: 'http://your-domain',
-        ghp: 'http://your-github-page'
+        aws: 'http://myxd.co.kr',
+        ghp: 'https://pyungwook-domfam.github.io/domfam012'
     },
     ip: {
         dev: "127.0.0.1",
-        production: "production-ip"
+        production: "13.209.55.219"
     },
     port: {
         dev: 3000,
         production: 80
     },
-    GA_TRACKING_ID: 'Your-GA_TRACKING_ID'
+    GA_TRACKING_ID: 'UA-158587547-1'
 };
 
 module.exports = {
