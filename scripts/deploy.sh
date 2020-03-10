b19a8c5568a4c69cdb68ad51d023e00228862b19
bohyeonp <219031@naver.com>
2020-03-10 19:33:42 +0900

    #141 자동화배포

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "> MyXD/_next 디렉토리 파일 제거"
-cd .
+cd ~
 cd MyXD/_next
 rm -rf *
 
 echo "> MyXD.zip 파일 이동"
 cd ../../zip
-mv MyXD.zip scripts appspec.yml ../MyXD/_next/
+mv MyXD.zip deploy.sh appspec.yml ../MyXD/_next/
 cd ../MyXD/_next/
 
 echo "> MyXD.zip 파일 압출 풀기"
 unzip MyXD.zip
-rm -rf MyXD.zip scripts appspec.yml
+rm -rf MyXD.zip deploy.sh appspec.yml
