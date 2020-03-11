a8044e914ac9d9c3e8f0605f41d653426d9c2418
bohyeonp <219031@naver.com>
2020-03-11 19:06:32 +0900

    #141 주석 변경

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-echo "> MyXD/_next 디렉토리 파일 제거"
+echo "> MyXD 디렉토리 기존 파일 제거"
 cd ~
 cd MyXD
 rm -rf *
 
-echo "> MyXD.zip 파일 이동"
+echo "> MyXD.zip 파일 이동 : zip → MyXD "
 cd ../zip
 mv * ../MyXD
 cd ../MyXD
 
 echo "> MyXD.zip 파일 압출 풀기"
 unzip MyXD.zip
+
+echo "> MyXD.zip 파일 제거"
 rm -rf MyXD.zip
+
+echo "> npm rebuild"
 npm rebuild
